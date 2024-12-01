49071003c33c564327394d65722a912083324e34
clementdrs <clementdrs.dev@gmail.com>
2024-12-01 17:28:20 +0100

    feat: add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,32 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import SignIn from "./pages/SignIn";
 import Kanban from "./pages/Kanban";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/kanban/:id" element={<Kanban/>} />
-        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/kanban/:id"
+          element={
+            <ProtectedRoute>
+              <Kanban />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
