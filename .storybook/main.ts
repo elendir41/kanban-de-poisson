c8dd8c9ae317963ecb5d7c8edd7e2548a3834956
Matteo Perisse <matteo.perisse@epita.fr>
2024-12-03 18:22:05 +0100

    feat: rest of the stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,6 +14,15 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      css: {
+        postcss: {
+          plugins: [tailwindcss, autoprefixer],
+        },
+      },
+    });
   },
 };
 export default config;
